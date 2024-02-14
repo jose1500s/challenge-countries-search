82fa4c00454c05df6b9972d8b1b18d28954384d8
Jose Antonio P <120035990@upq.edu.mx>
2024-02-13 18:01:37 -0600

    react router dom installed and layout component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Layout from './components/Layout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
