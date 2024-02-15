a433c400aab8c901ba32140edb3975151411003c
Jose Antonio P <120035990@upq.edu.mx>
2024-02-14 23:41:04 -0600

    filter by region

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,22 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 export default function Card({ countries }) {
-    
+    const { population, region, capital } = countries
+    const {svg, alt} = countries.flags
+    const formattedNumberDefault = population.toLocaleString();
     return (
         <div className="w-72 h-80 shadow-md py-2">
             <header className="h-1/2 w-full">
-                <LazyLoadImage 
+                <LazyLoadImage
                     className="h-full w-full object-cover"
-                    src={countries.flags.svg}
-                    alt={countries.flags.alt} />
+                    src={svg}
+                    alt={alt} />
             </header>
             <section className="p-5">
                 <h2 className="text-xl font-extrabold mb-3">{countries.name.common}</h2>
 
-                <p className="font-bold">Population: <span className="font-normal">{countries.population}</span></p>
-                <p className="font-bold">Region: <span className="font-normal">{countries.region}</span></p>
-                <p className="font-bold">Capital: <span className="font-normal">{countries.capital}</span></p>
+                <p className="font-bold">Population: <span className="font-normal">{formattedNumberDefault}</span></p>
+                <p className="font-bold">Region: <span className="font-normal">{region}</span></p>
+                <p className="font-bold">Capital: <span className="font-normal">{capital}</span></p>
             </section>
         </div>
     )
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,11 +3,13 @@ import Card from "../components/Card"
 import InputSearch from '../components/InputSearch'
 
 export default function Index() {
-    
+
     const [countriesData, setCountriesData] = useState([])
     const [search, setSearch] = useState('')
     const [filteredCountries, setFilteredCountries] = useState(countriesData)
-    
+
+    const [region, setRegion] = useState('')
+
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then((response) => response.json())
@@ -21,14 +23,44 @@ export default function Index() {
         setFilteredCountries(countriesData);
     }, [countriesData]);
 
+    const handleSelect = (e) => {
+        setRegion(e.target.value)
+    }
+
+    useEffect(() => {
+        if (!region) return; //evitar que se ejecute la llamada si region no tiene nada
+        fetch(`https://restcountries.com/v3.1/region/${region}`)
+            .then((response) => response.json())
+            .then((data) => setFilteredCountries(data))
+            .catch(error => {
+                console.log('error ', error)
+            })
+    }, [region])
+
+   
+
     return (
         <>
-            <InputSearch
-                search={search}
-                setSearch={setSearch}
-                setFilteredCountries={setFilteredCountries}
-                countriesData={countriesData}
-            />
+            <div className="flex justify-between items-center">
+                <InputSearch
+                    search={search}
+                    setSearch={setSearch}
+                    setFilteredCountries={setFilteredCountries}
+                    countriesData={countriesData}
+                />
+                <div className="space-y-0.5 w-36 mr-12">
+                    <label htmlFor="basic" className="text-base font-medium text-slate-500">Filter by region</label>
+                    <select id="basic" className="block w-full rounded-md p-1 text-lg transition placeholder:text-slate-400 focus:border-blue-600 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-slate-200 disabled:opacity-75 truncate undefined border-slate-300"
+                    onChange={handleSelect}
+                    >
+                        <option defaultValue="">Select</option>
+                        <option value="Africa">Africa</option>
+                        <option value="America">America</option>
+                        <option value="Europe">Europe</option>
+                        <option value="Oceania">Oceania</option>
+                    </select>
+                </div>
+            </div>
             <div className="flex flex-wrap justify-center items-center gap-4 mt-10">
                 {filteredCountries.length > 0 && filteredCountries.map((country) => (
                     <Card key={country.name.common} countries={country} />
